09a5b448883fb109939c6841d6a9784d0f45b241
voskanvl <voskanvl@gmail.com>
2022-12-07 17:25:40 +0300

    socials + scroll ts

--- src/main.ts
+++ src/main.ts
@@ -3,8 +3,11 @@ import "@splidejs/splide/css";
 
 import slides from "./slides";
 import roller from "./roller";
+import scroll from "./scroll";
 
 /* SLIDES */
 slides();
 /* ROLLER */
 roller();
+/* SCROLL */
+scroll();
