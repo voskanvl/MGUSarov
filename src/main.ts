59c08246b25333960db2da5cfeafc10d48315ce9
voskanvl <voskanvl@gmail.com>
2022-12-10 16:40:53 +0300

    fix interviews2

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,14 @@
-import "./sass/style.sass";
-import "@splidejs/splide/css";
+import './sass/style.sass';
+import '@splidejs/splide/css';
 
-import roller from "./roller";
+import roller from './roller';
 // import scroll from "./scroll";
-import scrollIO from "./scrollIO";
+import scrollIO from './scrollIO';
 
 /* SLIDES */
-roller();
+// roller();
 /* SCROLL */
 // scroll();
-scrollIO(document.querySelector<HTMLElement>(".interviews__right"));
+scrollIO(document.querySelector<HTMLElement>('.interviews__right'));
+scrollIO(document.querySelector<HTMLElement>('.interviews1__right'));
+scrollIO(document.querySelector<HTMLElement>('.interviews2__right'));
